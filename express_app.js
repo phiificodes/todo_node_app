c189f00ea40f463c79f6958b6580d6c43caf5e91
Fiifi Bernard <benasa58@gmail.com>
2021-12-21 16:48:53 +0000

    server_update

--- express_app.js
+++ express_app.js
@@ -9,23 +9,24 @@ const PORT = process.env.PORT || 4000;
 
 server.use(express.json());
 
+server.get("/", function (req, res) {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to phiificodes node.js api",
+  });
+});
+server.get("/todos", todoController.getAllTodos);
+server.get("/todo/:id", todoController.getTodoById);
+server.post("/todo", todoController.insertTodo);
+server.put("/todo/:id", todoController.updateTodoById);
+server.delete("/todo/:id", todoController.deleteTodoById);
+
 server.listen(PORT, function () {
   console.log("Server started,running in express...");
   mongoose
     .connect(process.env.MONGO_DB_ATLAS_TODOS_URL)
     .then(function () {
       console.log("DB is connected");
-      server.get("/", function (req, res) {
-        res.status(200).json({
-          success: true,
-          message: "Welcome to phiificodes node.js api",
-        });
-      });
-      server.get("/todos", todoController.getAllTodos);
-      server.get("/todo/:id", todoController.getTodoById);
-      server.post("/todo", todoController.insertTodo);
-      server.put("/todo/:id", todoController.updateTodoById);
-      server.delete("/todo/:id", todoController.deleteTodoById);
     })
     .catch((error) => {
       console.log("DB is not connected: " + error.message);
